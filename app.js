e59d60c0db9fd50eb6fd8f186150b894a616df13
Angela Mitchell <angela.mitchell@vmlyr.com>
2021-06-01 12:21:06 -0500

    use native event objct to access name value from input

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
+    // setName(event) {
+    //   this.name = event.target.value;
+    // },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
     add(num) {
-      // this.counter = this.counter + num;
       this.counter += num;
     },
     reduce(num) {
-      // this.counter = this.counter - num;
       this.counter -= num;
-    }
+    },
   }
 });
 
